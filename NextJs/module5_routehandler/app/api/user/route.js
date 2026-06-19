06f16a244426882ca584491c8324de8e92106a6f
Nitish Rai <nitishrairaj@gmail.com>
2026-06-19 13:33:07 +0530

    Cookie is cooked

--- NextJs/module5_routehandler/app/api/user/route.js
+++ NextJs/module5_routehandler/app/api/user/route.js
@@ -1,13 +1,22 @@
 // Get request handler
-import { headers } from "next/headers";
+import { headers, cookies } from "next/headers";
 export async function GET(req){
-    const reqHeader = await headers();
     // const reqHeader = new Headers(req.headers);
-    console.log(reqHeader.get("Authorization"));
+    // console.log(reqHeader.get("Authorization"));
+
+
+    // const username = req.cookies.get("username");
+    // console.log(username);
+
+    const cookieStore = await cookies();
+    cookieStore.set("theme","dark");
+    const theme = cookieStore.get("theme");
+    console.log(theme);
     return new Response("<h1>Hello world</h1>",
         {
             headers:{
-                "content-type":"text.html"
+                "content-type":"text.html",
+                "set-cookie":"username=nitish"
             }
         }
     );
