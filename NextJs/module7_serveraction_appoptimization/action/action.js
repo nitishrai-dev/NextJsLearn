2b6ca8e9539da2d3f34f18a671674d1d8a087604
Nitish Rai <nitishrairaj@gmail.com>
2026-06-25 21:54:41 +0530

    add server action

---
+++ NextJs/module7_serveraction_appoptimization/action/action.js
@@ -0,0 +1,20 @@
+"use server";
+export async function createTodo(formData){
+    const title = formData.get("title");
+    console.log("Creating a todo",title)
+} 
+
+export async function updateTodo(title, desc, isCompleted){
+    const newTodo = {
+        title,
+        desc,
+        isCompleted
+    }
+
+    // db action
+
+    return {
+        success:true,
+        message:"Updated successfully"
+    }
+}
